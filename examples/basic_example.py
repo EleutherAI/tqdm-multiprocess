42d44c7f273fa5ee2534d710aced512db5bae027
researcher2 <32028883+researcher2@users.noreply.github.com>
2020-09-30 01:15:31 +1000

    Update basic_example.py

--- examples/basic_example.py
+++ examples/basic_example.py
@@ -1,5 +1,6 @@
 from time import sleep
 import multiprocessing
+import tqdm
 
 import logging
 from tqdm_multiprocess.logger import setup_logger_tqdm
@@ -7,10 +8,10 @@ logger = logging.getLogger(__name__)
 
 from tqdm_multiprocess import TqdmMultiProcessPool
 
-def some_other_function(tqdm_func):
-    iterations1 = 100
-    iterations2 = 5
-    iterations3 = 2
+iterations1 = 100
+iterations2 = 5
+iterations3 = 2
+def some_other_function(tqdm_func, global_tqdm):
     
     total_iterations = iterations1 * iterations2 * iterations3
     with tqdm_func(total=total_iterations, dynamic_ncols=True) as progress3:
@@ -31,28 +32,35 @@ def some_other_function(tqdm_func):
                             progress1.update()
                             progress2.update()
                             progress3.update()
+                            global_tqdm.update()
 
     logger.warning(f"Warning test message. {multiprocessing.current_process().name}")
     logger.error(f"Error test message. {multiprocessing.current_process().name}")
 
         
 # Multiprocessed
-def example_multiprocessing_function(some_input, tqdm_func):  
+def example_multiprocessing_function(some_input, tqdm_func, global_tqdm):  
     logger.debug(f"Debug test message - I won't show up in console. {multiprocessing.current_process().name}")
     logger.info(f"Info test message. {multiprocessing.current_process().name}")
-    some_other_function(tqdm_func)
+    some_other_function(tqdm_func, global_tqdm)
     return True
 
-def error_callback():
+def error_callback(result):
     print("Error!")
+
+def done_callback(result):
+    print("Done. Result: ", result)
 
 def example():
     pool = TqdmMultiProcessPool()
     process_count = 4
     task_count = 10
     initial_tasks = [(example_multiprocessing_function, (i,)) for i in range(task_count)]    
-    results = pool.map(process_count, initial_tasks, error_callback)
-    print(results)
+    total_iterations = iterations1 * iterations2 * iterations3 * process_count
+    with tqdm.tqdm(total=total_iterations, dynamic_ncols=True) as global_progress:
+        global_progress.set_description("global")
+        results = pool.map(process_count, global_progress, initial_tasks, error_callback, done_callback)
+        print(results)
 
 if __name__ == '__main__':
     logfile_path = "tqdm_multiprocessing_example.log"
